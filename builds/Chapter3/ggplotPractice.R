5e2c0cabe7250be05b63d767da8c795a161297ac
WACKE127 <tylerwacker2021@gmail.com>
2024-12-20 14:40:46 -0600

    Added Shiny Support for Chapter3 to avoid issues with accessing filesystem

--- builds/Chapter3/ggplotPractice.R
+++ builds/Chapter3/ggplotPractice.R
@@ -25,14 +25,62 @@ names(CAT.MEDV.per.CHAS) <- c("CHAS", "MeanCATMEDV")
 CAT.MEDV.per.CHAS$CHAS <- factor(CAT.MEDV.per.CHAS$CHAS)
 
 #Line Chart
-plot(ridership.ts, 
+plots.1 <- function() {
+    plot(ridership.ts, 
     xlab="Year", 
     ylab="Ridership (in 000s)",
     ylim=c(1300,2300))
+}
 
 #Scatter Plot of LSTAT vs MEDV
-plot(housing.df$MEDV ~ housing.df$LSTAT,
+plots.2 <- function() {
+    plot(housing.df$MEDV ~ housing.df$LSTAT,
     xlab="LSTAT",
     ylab="MEDV")
+}
 
-#Barchart 
+#Barchart of CHAS vs % mean MEDV
+plots.3 <- function() {
+    barplot(MEDV.per.CHAS$MeanMEDV,
+    names.arg=MEDV.per.CHAS$CHAS,
+    xlab="CHAS",
+    ylab="Avg. MEDV")
+}
+
+#Barchart of CHAS vs. $ CAT.MEDV
+plots.4 <- function() {
+    barplot(CAT.MEDV.per.CHAS$MeanCATMEDV * 100, 
+    names.arg=CAT.MEDV.per.CHAS$CHAS,
+    xlab="CHAS",
+    ylab="% of CAT.MEDV")
+}
+
+#Display plots in Shiny Webserver
+library(shiny)
+
+ui <- fluidPage(
+    plotOutput("plot1"),
+    plotOutput("plot2"),
+    plotOutput("plot3"),
+    plotOutput("plot4")
+)
+
+server <- function(input, output) {
+    output$plot1 <- renderPlot({
+        plots.1()
+    })
+    
+    output$plot2 <- renderPlot({
+        plots.2()
+    })
+
+    output$plot3 <- renderPlot({
+        plots.3()
+    })
+
+    output$plot4 <- renderPlot({
+        plots.4()
+    })
+}
+
+shinyApp(ui = ui, server = server)
