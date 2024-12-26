a494412776918f49a79950db8d6d2bc4d85c8eeb
WACKE127 <tylerwacker2021@gmail.com>
2024-12-26 09:10:02 -0600

    Unconfused

--- builds/Chapter5/confusionMatrixPractice.R
+++ builds/Chapter5/confusionMatrixPractice.R
@@ -1,19 +1,32 @@
+# I am confused by this.
+
 library(caret)
 
 setwd("../../mlba/mlba/data-raw")
 owner.df <- read.csv("ownerExample.csv.gz")
 
-confusionMatrix(factor(ifelse(owner.df$Probability>0.5,
+#Factor is data structure for categories
+#Levels are unique categories
+#confusionMatrix expects levels to match perfectly
+#PRINT LEVELS TO CONSOLE
+levels(owner.df$Class)
+
+owner.df$Class <- factor(owner.df$Class, levels = c("nonowner", "owner"))
+
+#PRINT LEVELS AGAIN
+levels(owner.df$Class)
+
+confusionMatrix(factor(ifelse(owner.df$Probability > 0.5,
                       "owner",
                       "nonowner")),
                       owner.df$Class)$table
                       
-confusionMatrix(factor(ifelse(owner.df$Probability>0.25,
+confusionMatrix(factor(ifelse(owner.df$Probability > 0.25,
                       "owner",
                       "nonowner")),
                       owner.df$Class)$table
 
-confusionMatrix(factor(ifelse(owner.df$Probability>0.75,
+confusionMatrix(factor(ifelse(owner.df$Probability > 0.75,
                       "owner",
                       "nonowner")),
                       owner.df$Class)$table
