d8bf35bef6630a3ce8dff9e261c837a1e52cdf64
WACKE127 <tjwac27@gmail.com>
2025-01-17 18:35:36 -0600

    C8 bayesPractice WIP

---
+++ builds/Chapter8/bayesPractice.R
@@ -0,0 +1,17 @@
+library(tidyverse)
+library(caret)
+library(e1071)
+
+setwd("../../mlba/mlba/data-raw")
+delays.df <- read.csv("FlightDelays.csv.gz") %>%
+#IMPORTANT, numerical variables must be categorical
+             mutate(
+                DAY_WEEK = factor(DAY_WEEK),
+                ORIGIN = factor(ORIGIN),
+                DEST = factor(DEST),
+                CARRIER = factor(CARRIER),
+                Flight.Status = factor(Flight.Status),
+                CRS_DEP_TIME = factor(round(CRS_DEP_TIME/100))
+             ) %>%
+             select(DAY_WEEK, CRS_DEP_TIME, ORIGIN, DEST, CARRIER, Flight.Status)
+
